5f5def927cdb723a3fdeab0f64cb7d055fc82ed0
amanbarbaria <amanbarbaria@gmail.com>
2023-09-01 19:39:52 +0530

    Highlight the selected text

--- service_worker/background.js
+++ service_worker/background.js
@@ -1,38 +1,148 @@
-function injectedFunction(tab, info, color) {
-  console.log("executeScript via function",info, tab, color);
-  console.log("selectionText", info.selectionText);
-  // console.log("pageUrl",info.pageUrl);
-  // console.log("favIconUrl", tab.favIconUrl);
-  // console.log("title", tab.title);
-  // console.log("height", tab.height);
-  // console.log("width", tab.width);
-  // console.log("highlighted", tab.highlighted);
-  // console.log("selected", tab.selected);
-  // console.log("id", tab.id);
-  // console.log("index", tab.index);
 
-  // document.body.style.backgroundColor = color;
-  // Logged in current tab's console
-  console.log("--------------executeScript via function--------------");
-  const data = document.getElementById("ember25")
-  console.log("--------------data--------------", data);
+function injectedFunction(tab, info) {
+  let selection1 = window.getSelection();
+  console.log("--------------selection1--------------",selection1);
 
-  const selection = window.getSelection();
-  console.log("--------------selection--------------",selection);
+  let anchorNode = selection1.anchorNode;
 
-  const activeElement = document.activeElement;
-  console.log("--------------activeElement--------------",activeElement);
+  let identifier = {};
+  let selectionNodeData = {
+      textContent: selection1.toString(), 
+      anchorOffset: selection1.anchorOffset,
+      focusOffset: selection1.focusOffset,
+      contextBefore: selection1.anchorNode.textContent.slice(0, selection1.anchorOffset),
+      contextAfter: selection1.focusNode.textContent.slice(selection1.focusOffset),
+    };
+    identifier["selectionNodeData"] = selectionNodeData;
+    identifier["idLevel"] = -1;
+    identifier["maxLevel"] = -1;
+
+
+    let nodePaths = {};
+    let parentLevel = 0;
+  // Navigate up to find the closest element with an ID
+  while (anchorNode) {
+    console.log("parentLevel", parentLevel);
+    console.log("anchorNode", anchorNode);
+    let nodePath = {};
+    nodePath["id"] = anchorNode.id;
+    nodePath["nodeType"] = anchorNode.nodeType;
+    nodePath["className"] = anchorNode.className // 'stack type'
+    nodePath["nodeName"] = anchorNode.nodeName // 'DIV'
+    nodePath["localName"] = anchorNode.localName // 'div
+    nodePath["tagName"] = anchorNode.tagName // 'DIV'
+
+    nodePath["prevSiblingCount"] = 0;    
+    let siblingNode = anchorNode.previousSibling;
+    while (siblingNode && siblingNode != null) {
+      nodePath["prevSiblingCount"] = nodePath["prevSiblingCount"] + 1;
+      siblingNode = siblingNode.previousSibling;
+    }
+    
+    nodePaths[parentLevel] = nodePath;
+    
+    if (nodePath["id"]) {
+      identifier["idLevel"] = parentLevel;
+      break;
+    }else{
+      anchorNode = anchorNode.parentNode;
+      parentLevel++;
+    }
+  }
+  if (!identifier["idLevel"]){
+    identifier["maxLevel"] = parentLevel - 1;
+  }
+
+  identifier["nodePaths"] = nodePaths;
+  
+  console.log("--------------identifier--------------",JSON.stringify(identifier));
+//   return identifier;  
+// }
+
+// function injectedFunction1() {
+  // identifier = {"selectionNodeData":{"textContent":"extension","anchorOffset":18,"focusOffset":27,"contextBefore":"Create your first ","contextAfter":" that inserts a new element on the page."},"idLevel":6,"maxLevel":-1,"nodePaths":{"0":{"nodeType":3,"nodeName":"#text","prevSiblingCount":0},"1":{"id":"","nodeType":1,"className":"type--h5","nodeName":"P","localName":"p","tagName":"P","prevSiblingCount":1},"2":{"id":"","nodeType":1,"className":"flow-space-200 stack","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":0},"3":{"id":"","nodeType":1,"className":"measure-long pad-left-400 pad-right-400 stack width-full","nodeName":"ARTICLE","localName":"article","tagName":"ARTICLE","prevSiblingCount":0},"4":{"id":"","nodeType":1,"className":"display-flex justify-content-center width-full","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":1},"5":{"id":"","nodeType":1,"className":"display-flex gap-top-300 lg:gap-top-400","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":2},"6":{"id":"main-content","nodeType":1,"className":"","nodeName":"MAIN","localName":"main","tagName":"MAIN","prevSiblingCount":5}}};
+  console.log("--------------Highlight selection start--------------");
+
+  let inputNodePaths = identifier["nodePaths"];
+  
+  let idLevel = identifier["idLevel"];
+  
+  if (idLevel < 0 ) {
+    console.log("--------------idLevel is undefined--------------");
+    return;
+  }
+  
+  let currentLevel = idLevel;
+  let currentNodePath = inputNodePaths[currentLevel];
+  
+  let currentElement = document.getElementById(currentNodePath.id);
+  
+  while(currentLevel > 0){
+    if (!currentElement){
+      console.log("--------------Missing currentElement--------------", currentLevel);
+    }
+    let nextNodePath = inputNodePaths[currentLevel - 1];
+    let nextElement = currentElement.childNodes[nextNodePath.prevSiblingCount];
+    currentElement = nextElement;
+    currentLevel--;
+  }
+  let inputSelectionNodeData = identifier["selectionNodeData"];
+  let startOffset = inputSelectionNodeData.anchorOffset
+  let endOffset = inputSelectionNodeData.focusOffset
+  let styleClass = "highlighted";
+
+    // Get the element's text content
+    let originalText = currentElement.textContent;
+      // Check for valid offsets
+  if (startOffset >= endOffset || startOffset < 0 || endOffset > originalText.length) {
+    console.log("Invalid offsets!");
+    return;
+  }
+   
+  // let cloneCurrentElement = currentElement.cloneNode(true);
+
+  if (currentElement.nodeType === Node.ELEMENT_NODE) {
+    // Slice the text into three parts: before, highlight, and after
+      let beforeText = originalText.slice(0, startOffset);
+      let highlightText = originalText.slice(startOffset, endOffset);
+      let afterText = originalText.slice(endOffset);
+
+    // Create new HTML with the highlight text wrapped in a span
+    let highlightedHTML = `
+      ${beforeText}
+      <span class="#{styleClass}" style="background-color:yellow;">${highlightText}</span>
+      ${afterText}
+    `;
+
+      // Replace the element's content
+      currentElement.innerHTML = highlightedHTML;
+  } else if (currentElement.nodeType === Node.TEXT_NODE) {
+    let textNode = currentElement;
+    
+    // Split the text node at the ending offset, moving text after the endOffset into a new text node.
+    let endNode = textNode.splitText(endOffset);
+    
+    // Split the text node at the starting offset, moving text after the startOffset into a new text node.
+    let middleNode = textNode.splitText(startOffset);
+    
+    // Now, middleNode contains the text to be highlighted. Create a new element to wrap it.
+    let highlightSpan = document.createElement("span");
+    highlightSpan.style.backgroundColor = "yellow";
+    highlightSpan.className = styleClass;
+
+    // Replace middleNode with highlightSpan, putting middleNode inside highlightSpan.
+    highlightSpan.appendChild(middleNode);
+    textNode.parentNode.insertBefore(highlightSpan, endNode);
+
+  } else {
+    console.log("--------------currentElement is not a handled node type--------------", currentElement.nodeType);
+    return;
+  }
+  
+
 }
 
-// On Action Button Click
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     files: ["content_scripts/content.js"]
-//   });
-// });
 
-// chrome.runtime.onInstalled.addListener(async () => {
 // Create a context menu for selected text
 chrome.contextMenus.create({
     id: "annotateText",
@@ -42,21 +152,6 @@ chrome.contextMenus.create({
   
   // Add a click event listener
   chrome.contextMenus.onClicked.addListener((info, tab) => {
-
-//   chrome.windows.create({
-//     width: 350,
-//     height: 250,
-//     top: 200,
-//     left: 400,
-//     type: "popup",
-//     url: "alert.html"
-//   });   
-// Service worker cannot access the DOM, so alert() isn't available.
-
-
-
-
-
     if (info.menuItemId === "annotateText") {
       // Or call another file.
       // chrome.scripting.executeScript({
@@ -67,43 +162,7 @@ chrome.contextMenus.create({
       chrome.scripting.executeScript({
         target : {tabId : tab.id},
         func : injectedFunction,
-        args : [tab, info, "yellow"]
+        args : [tab, info]
       });
     }
- 
-    // console.log("--------------annotateText--------------");
-  //     chrome.scripting.executeScript({
-  //       target: {tabId: tab.id},
-  //       function: () => {
-
-  //         let node = selection.anchorNode;
-  //         console.log("--------------node--------------",node);
-  //         // Navigate up the DOM tree to find the closest element with an ID
-  //         while (node && node.nodeType !== Node.ELEMENT_NODE) {
-  //           console.log("--------------node.nodeType--------------",node.id,node.nodeType, node);
-  //           node = node.parentNode;
-  //         }
-  
-  //         let elementWithId = null;
-  
-  //         while (node) {
-  //           if (node.id) {
-  //             elementWithId = node;
-  //             break;
-  //           }
-  //           node = node.parentNode;
-  //         }
-  
-  //         return elementWithId ? elementWithId.id : null;
-  //       }
-  //     }, (result) => {
-  //       if (chrome.runtime.lastError) {
-  //         console.error(chrome.runtime.lastError);
-  //       } else {
-  //         console.log(`ID of element or nearest parent with an ID: ${result[0]?.result}`);
-  //       }
-  //     });
-  //   }
-  // });
-
 });
---
+++ service_worker/fallback.js
@@ -0,0 +1,167 @@
+
+function injectedFunction(tab, info, color) {
+  let selection1 = window.getSelection();
+  console.log("--------------selection1--------------",selection1);
+
+  // let anchorNode = selection1.anchorNode;
+
+  // let identifier = {};
+  // let selectionNodeData = {
+  //     textContent: selection1.toString(), 
+  //     anchorOffset: selection1.anchorOffset,
+  //     focusOffset: selection1.focusOffset,
+  //     contextBefore: selection1.anchorNode.textContent.slice(0, selection1.anchorOffset),
+  //     contextAfter: selection1.focusNode.textContent.slice(selection1.focusOffset),
+  //   };
+  //   identifier["selectionNodeData"] = selectionNodeData;
+  //   identifier["idLevel"] = -1;
+  //   identifier["maxLevel"] = -1;
+
+  //   let nodePaths = {};
+  //   let parentLevel = 0;
+  // // Navigate up to find the closest element with an ID
+  // while (anchorNode) {
+  //   console.log("parentLevel", parentLevel);
+  //   console.log("anchorNode", anchorNode);
+  //   let nodePath = {};
+  //   nodePath["id"] = anchorNode.id;
+  //   nodePath["nodeType"] = anchorNode.nodeType;
+  //   nodePath["className"] = anchorNode.className // 'stack type'
+  //   nodePath["nodeName"] = anchorNode.nodeName // 'DIV'
+  //   nodePath["localName"] = anchorNode.localName // 'div
+  //   nodePath["tagName"] = anchorNode.tagName // 'DIV'
+
+  //   nodePath["prevSiblingCount"] = 0;    
+  //   let siblingNode = anchorNode.previousSibling;
+  //   while (siblingNode && siblingNode != null) {
+  //     nodePath["prevSiblingCount"] = nodePath["prevSiblingCount"] + 1;
+  //     siblingNode = siblingNode.previousSibling;
+  //   }
+    
+  //   nodePaths[parentLevel] = nodePath;
+    
+  //   if (nodePath["id"]) {
+  //     identifier["idLevel"] = parentLevel;
+  //     break;
+  //   }else{
+  //     anchorNode = anchorNode.parentNode;
+  //     parentLevel++;
+  //   }
+  // }
+  // if (!identifier["idLevel"]){
+  //   identifier["maxLevel"] = parentLevel - 1;
+  // }
+
+  // identifier["nodePaths"] = nodePaths;
+  
+  // console.log("--------------identifier--------------",JSON.stringify(identifier));
+  // return identifier;  
+}
+
+// function injectedFunction1() {
+//   identifier = {"selectionNodeData":{"textContent":"extension","anchorOffset":18,"focusOffset":27,"contextBefore":"Create your first ","contextAfter":" that inserts a new element on the page."},"idLevel":6,"maxLevel":-1,"nodePaths":{"0":{"nodeType":3,"nodeName":"#text","prevSiblingCount":0},"1":{"id":"","nodeType":1,"className":"type--h5","nodeName":"P","localName":"p","tagName":"P","prevSiblingCount":1},"2":{"id":"","nodeType":1,"className":"flow-space-200 stack","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":0},"3":{"id":"","nodeType":1,"className":"measure-long pad-left-400 pad-right-400 stack width-full","nodeName":"ARTICLE","localName":"article","tagName":"ARTICLE","prevSiblingCount":0},"4":{"id":"","nodeType":1,"className":"display-flex justify-content-center width-full","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":1},"5":{"id":"","nodeType":1,"className":"display-flex gap-top-300 lg:gap-top-400","nodeName":"DIV","localName":"div","tagName":"DIV","prevSiblingCount":2},"6":{"id":"main-content","nodeType":1,"className":"","nodeName":"MAIN","localName":"main","tagName":"MAIN","prevSiblingCount":5}}};
+//   console.log("--------------Highlight selection1 start--------------",identifier);
+
+//   let nodePaths = identifier["nodePaths"];
+  
+//   let idLevel = identifier["idLevel"];
+  
+//   if (idLevel < 0 ) {
+//     console.log("--------------idLevel is undefined--------------");
+//     return;
+//   }
+  
+//   let currentLevel = idLevel;
+//   let currentNodePath = nodePaths[currentLevel];
+  
+//   let currentElement = document.getElementById(currentNodePath.id);
+  
+//   while(currentLevel > 0){
+//     if (!currentElement){
+//       console.log("--------------Missing currentElement--------------", currentLevel);
+//     }
+//     let nextNodePath = nodePaths[currentLevel - 1];
+//     let nextElement = currentElement.childNodes[nextNodePath.prevSiblingCount];
+//     currentElement = nextElement;
+//     currentLevel--;
+//   }
+//   let selectionNodeData = identifier["selectionNodeData"];
+//   let startOffset = selectionNodeData.anchorOffset
+//   let endOffset = selectionNodeData.focusOffset
+//   let styleClass = "highlighted";
+
+//     // Get the element's text content
+//     let originalText = currentElement.textContent;
+//       // Check for valid offsets
+//   if (startOffset >= endOffset || startOffset < 0 || endOffset > originalText.length) {
+//     console.log("Invalid offsets!");
+//     return;
+//   }
+   
+//   // let cloneCurrentElement = currentElement.cloneNode(true);
+
+//   if (currentElement.nodeType === Node.ELEMENT_NODE) {
+//     // Slice the text into three parts: before, highlight, and after
+//       let beforeText = originalText.slice(0, startOffset);
+//       let highlightText = originalText.slice(startOffset, endOffset);
+//       let afterText = originalText.slice(endOffset);
+
+//     // Create new HTML with the highlight text wrapped in a span
+//     let highlightedHTML = `
+//       ${beforeText}
+//       <span class="#{styleClass}" style="background-color:yellow;">${highlightText}</span>
+//       ${afterText}
+//     `;
+
+//       // Replace the element's content
+//       currentElement.innerHTML = highlightedHTML;
+//   } else if (currentElement.nodeType === Node.TEXT_NODE) {
+//     let textNode = currentElement;
+    
+//     // Split the text node at the ending offset, moving text after the endOffset into a new text node.
+//     let endNode = textNode.splitText(endOffset);
+    
+//     // Split the text node at the starting offset, moving text after the startOffset into a new text node.
+//     let middleNode = textNode.splitText(startOffset);
+    
+//     // Now, middleNode contains the text to be highlighted. Create a new element to wrap it.
+//     let highlightSpan = document.createElement("span");
+//     highlightSpan.style.backgroundColor = "yellow";
+//     highlightSpan.className = styleClass;
+
+//     // Replace middleNode with highlightSpan, putting middleNode inside highlightSpan.
+//     highlightSpan.appendChild(middleNode);
+//     textNode.parentNode.insertBefore(highlightSpan, endNode);
+
+//   } else {
+//     console.log("--------------currentElement is not a handled node type--------------", currentElement.nodeType);
+//     return;
+//   }
+  
+
+// }
+
+
+// Create a context menu for selected text
+chrome.contextMenus.create({
+    id: "annotateText",
+    title: "Annotate Text",
+    contexts: ["selection"]
+  });
+  
+  // Add a click event listener
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "annotateText") {
+      // Or call another file.
+      // chrome.scripting.executeScript({
+      //   target: { tabId: tab.id },
+      //   files: ["content_scripts/add_comment.js"]
+      // });
+
+      chrome.scripting.executeScript({
+        target : {tabId : tab.id},
+        func : injectedFunction,
+        args : [tab, info, "yellow"]
+      });
+    }
+});
